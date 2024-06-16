f8dc6f5c5618df1c3fdc5d5056760d2d70c1040c
chris <103071318+cgik@users.noreply.github.com>
2024-06-15 20:09:57 -0700

    Update Header.js

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function Header() {
     return (
         <div className="navbar bg-base-100">
+            <script defer data-domain="tetrigo.ris.gg" src="https://plausible.io/js/script.js"></script>
             <div className="flex-none">
                 <button className="btn btn-square btn-ghost">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
