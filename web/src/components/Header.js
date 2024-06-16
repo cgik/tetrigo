44dfabd10e14cadd872989cbd7b2ccb5dfd7420b
chris <103071318+cgik@users.noreply.github.com>
2024-06-15 20:24:00 -0700

    chore: more header adjustments

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,26 +1,53 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Header() {
-    return (
-      <div>
-        <Head>
-          <script defer data-domain="tetrigo.ris.gg" src="https://plausible.io/js/script.js"></script>
-          <div className="navbar bg-base-100">
-              <div className="flex-none">
-                  <button className="btn btn-square btn-ghost">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                  </button>
-              </div>
-              <div className="flex-1">
-                  <a className="btn btn-ghost text-xl">tetrigo</a>
-              </div>
-              <div className="flex-none">
-                  <button className="btn btn-square btn-ghost">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-                  </button>
-              </div>
-          </div>
-        <Head>
+  return (
+    <Head>
+      <script
+        defer
+        data-domain="tetrigo.ris.gg"
+        src="https://plausible.io/js/script.js"
+      ></script>
+      <div className="navbar bg-base-100">
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 h-5 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">tetrigo</a>
+        </div>
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 h-5 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+              ></path>
+            </svg>
+          </button>
+        </div>
       </div>
-    );
+    </Head>
+  );
 }
